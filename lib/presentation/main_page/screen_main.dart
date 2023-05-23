519b9ce2da595ce9fb18b89a4ee887aae3cff7fe
Athulprakash02 <athulkichu02@gmail.com>
2023-05-23 18:34:37 +0530

    bottom nav added

---
+++ lib/presentation/main_page/screen_main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:netflix/presentation/downloads/screen_downloads.dart';
+import 'package:netflix/presentation/home/screen_home.dart';
+import 'package:netflix/presentation/main_page/widgets/bottom_nav.dart';
+import 'package:netflix/presentation/new_and_hot/screen_new_and_hot.dart';
+import 'package:netflix/presentation/search/screen_search.dart';
+
+class MainScreen extends StatelessWidget {
+   MainScreen({super.key});
+
+  final _pages = [
+    ScreenHome(),
+    ScreenNewAndHot(),
+    ScreenSearch(),
+    ScreenDownloads(),
+
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      
+      body: SafeArea(
+        child: ValueListenableBuilder(valueListenable: indexChangeNotifier, builder: (context, index, _) {
+          return _pages[index];
+        },),
+      ),
+      bottomNavigationBar: BottomNavigationWidget(),
+    );
+  }
+}
---
+++ lib/presentation/main_page/widgets/bottom_nav.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:netflix/core/colors/colors.dart';
+
+ValueNotifier<int> indexChangeNotifier = ValueNotifier(0);
+
+class BottomNavigationWidget extends StatelessWidget {
+  const BottomNavigationWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: indexChangeNotifier,
+      builder: (context, newValue, _) {
+        return BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: newValue,
+          onTap: (index) {
+            indexChangeNotifier.value = index;
+          },
+          backgroundColor: backgroundColor,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          selectedIconTheme: const IconThemeData(color: Colors.white),
+          unselectedIconTheme: const IconThemeData(
+            color: Colors.grey,
+          ),
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                ),
+                label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.video_collection_outlined,
+                ),
+                label: 'New & Hot'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.search,
+                ),
+                label: 'Search'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.arrow_circle_down,
+                ),
+                label: 'Downloads'),
+          ],
+        );
+      },
+    );
+  }
+}
